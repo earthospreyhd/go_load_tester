4c144f733c4d894dd32f0bbeea585df8184bfbbd
Zach Leong <earthospreyhd@gmail.com>
2019-04-24 20:51:18 +1000

    Improved formatting and removed unnecessary code

--- loadtester.go
+++ loadtester.go
@@ -1,23 +1,33 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"os"
     "net/http"
     "time"
 )
 
-const url = "http://localhost:5050"
 //Number of individual threads to make requests
 const NUMTHREADS = 100
+const DEFAULT_URL = "http://localhost:5050"
 
 func main () {
+
+	cmdArguments := os.Args[1:]
+	for _, element := range cmdArguments {
+		if element == "-c" {
+			fmt.Println("we have found an option!")
+		}
+	}
+
+	url := DEFAULT_URL
     //Channel for threads to send their response times
     timeChannel := make (chan time.Duration)
     //spawn threads
     for i := 0; i < NUMTHREADS; i++ {
-        go makeResponse(timeChannel)
+        go makeResponse(timeChannel, url)
     }
-    var averageTime time.Duration
+    var averageTime time.Duration 
     //receive response times
     for i := 0; i < NUMTHREADS; i++ {
         select {
@@ -30,26 +40,20 @@ func main () {
 
 }
 
-func makeResponse (timeChannel chan time.Duration) {
+func makeResponse (timeChannel chan time.Duration, url string) {
     timeStart := time.Now()
     resp, err := http.Get(url)
     requestTime := time.Since(timeStart)
+    //Close http request after function is completed
+	defer resp.Body.Close()
 
     if err != nil {
         fmt.Println("error with request", err)
         return
     }
 
-    //Close http request after function is completed
-    defer resp.Body.Close()
-
-    if err != nil {
-        fmt.Println("error with reading request")
-        return
-    }
     fmt.Println("Made request in ", requestTime)
     //send response time on channel
     timeChannel <- requestTime
-
     return
 }
